d8c213f51a48d5bd4a36b36ab3d5af15d958a7d4
Bryce Inouye <binouye@ucsb.edu>
2025-04-28 19:26:53 -0700

    bi - articles step 3

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,40 @@
+const articlesFixtures = {
+  oneArticle: {
+    id: 1,
+    title: "Here’s the latest Nintendo Switch Online news from April!",
+    url: "https://www.nintendo.com/us/whatsnew/heres-the-latest-nintendo-switch-online-news-from-april-2025/",
+    explanation: "Game updates, special offers, and Nintendo music.",
+    email: "binouye@ucsb.edu",
+    dateAdded: "2025-04-28T00:00:00",
+  },
+  threeArticles: [
+    {
+      id: 1,
+      title: "Here’s the latest Nintendo Switch Online news from April!",
+      url: "https://www.nintendo.com/us/whatsnew/heres-the-latest-nintendo-switch-online-news-from-april-2025/",
+      explanation: "Game updates, special offers, and Nintendo music.",
+      email: "binouye@ucsb.edu",
+      dateAdded: "2025-04-28T00:00:00",
+    },
+    {
+      id: 2,
+      title: "RELEASE NOTES: THE BATTLE FOR KATANA KINGDOM!",
+      url: "https://supercell.com/en/games/brawlstars/blog/release-notes/release-notes-the-battle-for-katana-kingdom/",
+      explanation:
+        "The First Ultra Legendary Brawler Kaze, a Brawl MOBA, Wasabi Powers, Jae-yong the Karaoke King, and More!",
+      email: "brawlstars@ucsb.edu",
+      dateAdded: "2025-04-25T00:00:00",
+    },
+    {
+      id: 3,
+      title: "Patch 25.08 Notes",
+      url: "https://www.leagueoflegends.com/en-us/news/game-updates/patch-25-08-notes/",
+      explanation:
+        "Here comes patch 25.08! Bringing some balance changes across all the currently active queues. This is also the last patch of the current Ranked Season so make sure to get your 15 wins to lock in Victorious Twisted Fate!",
+      email: "riotgames@ucsb.edu",
+      dateAdded: "2025-04-15T00:00:00",
+    },
+  ],
+};
+
+export { articlesFixtures };
