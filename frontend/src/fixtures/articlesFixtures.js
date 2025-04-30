49fdfae0321213e6da98e8822f31994592b579b0
Bryce Inouye <binouye@ucsb.edu>
2025-04-29 21:01:46 -0700

    Fix fixtures

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,30 +1,42 @@
-const ucsbDatesFixtures = {
-  oneDate: {
+const articlesFixtures = {
+  oneArticle: {
     id: 1,
-    quarterYYYYQ: "20221",
-    name: "Noon on January 2nd",
-    localDateTime: "2022-01-02T12:00:00",
+    title: "Patch 25.09 Notes",
+    url: "https://www.leagueoflegends.com/en-us/news/game-updates/patch-25-09-notes/",
+    explanation:
+      "Welcome to Season 2 where we go to Ionia just in time for the Spirit Blossom Festival! We’ve got lots to celebrate this patch, so make sure to read the full patch notes for all the details!",
+    email: "riotgames@ucsb.edu",
+    dateAdded: "2025-04-29T12:00:00",
   },
-  threeDates: [
+  threeArticles: [
     {
       id: 1,
-      quarterYYYYQ: "20221",
-      name: "Noon on January 2nd",
-      localDateTime: "2022-01-02T12:00:00",
+      title: "Patch 25.09 Notes",
+      url: "https://www.leagueoflegends.com/en-us/news/game-updates/patch-25-09-notes/",
+      explanation:
+        "Welcome to Season 2 where we go to Ionia just in time for the Spirit Blossom Festival! We’ve got lots to celebrate this patch, so make sure to read the full patch notes for all the details!",
+      email: "riotgames@ucsb.edu",
+      dateAdded: "2025-04-29T12:00:00",
     },
     {
       id: 2,
-      quarterYYYYQ: "20222",
-      name: "Noon on April 3rd",
-      localDateTime: "2022-04-03T12:00:00",
+      title: "RELEASE NOTES: THE BATTLE FOR KATANA KINGDOM!",
+      url: "https://supercell.com/en/games/brawlstars/blog/release-notes/release-notes-the-battle-for-katana-kingdom/",
+      explanation:
+        "The First Ultra Legendary Brawler Kaze, a Brawl MOBA, Wasabi Powers, Jae-yong the Karaoke King, and More!",
+      email: "brawlstars@ucsb.edu",
+      dateAdded: "2025-04-25T11:11:11",
     },
     {
       id: 3,
-      quarterYYYYQ: "20223",
-      name: "Noon on July 4th",
-      localDateTime: "2022-07-04T12:00:00",
+      title: "Taco Bell Brings Back Fan-Favorite Chicken Nuggets",
+      url: "https://www.tacobell.com/newsroom/crispy-chicken-nuggets-return",
+      explanation:
+        "Following a sell-out launch and overwhelming fan demand, Crispy Chicken Nuggets return to menus on April 24 as one delicious way Taco Bell is exploring its long-term crispy chicken menu",
+      email: "tacobell@ucsb.edu",
+      dateAdded: "2025-04-17T12:12:12",
     },
   ],
 };
 
-export { ucsbDatesFixtures };
+export { articlesFixtures };
