ff72eeb568349c7dceff723dc7eea36dd1ffc84f
davidduena <131146357+davidduena@users.noreply.github.com>
2025-04-30 21:02:31 -0700

    dd - WP 2/4 fields again

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,43 +1,42 @@
 const helpRequestFixtures = {
-    oneHelpRequest: {
-        "id": 2,
-        "requesterEmail": "joe@gmail.com",
-        "teamId": "A",
-        "tableOrBreakoutRoom": "table",
-        "requestTime": "2025-12-10T06:34:12",
-        "explanation": "need help!",
-        "solved": false
-      },
-    threeHelpRequest: [
-        {
-          "id": 3,
-          "requesterEmail": "bob@ucsb.edu",
-          "teamId": "B",
-          "tableOrBreakoutRoom": "bRoom",
-          "requestTime": "2025-04-10T03:34:12",
-          "explanation": "need a lot of help!!",
-          "solved": true
-        },
-         {
-          "id": 4,
-          "requesterEmail": "dave@ucsb.edu",
-          "teamId": "C",
-          "tableOrBreakoutRoom": "bRoom",
-          "requestTime": "2025-04-18T07:44:14",
-          "explanation": "need a ton of help!!",
-          "solved": false
-        },
-        {
-          "id": 2,
-          "requesterEmail": "joe@gmail.com",
-          "teamId": "A",
-          "tableOrBreakoutRoom": "table",
-          "requestTime": "2025-12-10T06:34:12",
-          "explanation": "need help!",
-          "solved": false
-        }
-      ]
-  };
-  
-  export { helpRequestFixtures };
-  
+  oneHelpRequest: {
+    id: 2,
+    requesterEmail: "joe@gmail.com",
+    teamId: "A",
+    tableOrBreakoutRoom: "table",
+    requestTime: "2025-12-10T06:34:12",
+    explanation: "need help!",
+    solved: false,
+  },
+  threeHelpRequest: [
+    {
+      id: 3,
+      requesterEmail: "bob@ucsb.edu",
+      teamId: "B",
+      tableOrBreakoutRoom: "bRoom",
+      requestTime: "2025-04-10T03:34:12",
+      explanation: "need a lot of help!!",
+      solved: true,
+    },
+    {
+      id: 4,
+      requesterEmail: "dave@ucsb.edu",
+      teamId: "C",
+      tableOrBreakoutRoom: "bRoom",
+      requestTime: "2025-04-18T07:44:14",
+      explanation: "need a ton of help!!",
+      solved: false,
+    },
+    {
+      id: 2,
+      requesterEmail: "joe@gmail.com",
+      teamId: "A",
+      tableOrBreakoutRoom: "table",
+      requestTime: "2025-12-10T06:34:12",
+      explanation: "need help!",
+      solved: false,
+    },
+  ],
+};
+
+export { helpRequestFixtures };
