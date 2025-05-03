dcb6e7249562d89a088bfe3454dcd661b6454f58
Yue Cao <yuecao365@ucsb.edu>
2025-05-03 15:23:35 -0700

    yc - add tests for UCSBOrganization Placeholder pages for Index, Create, Edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,11 @@ function App() {
         )}
         {hasRole(currentUser, "ROLE_USER") && (
           <>
-            <Route exact path="/ucsborganization" element={<UCSBOrganizationIndexPage />} />
+            <Route
+              exact
+              path="/ucsborganization"
+              element={<UCSBOrganizationIndexPage />}
+            />
           </>
         )}
         {hasRole(currentUser, "ROLE_ADMIN") && (
