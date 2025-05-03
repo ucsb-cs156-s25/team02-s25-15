9062ef3b6d739d7e0b35f22800cbb4b344405db4
Yue Cao <yuecao365@ucsb.edu>
2025-05-02 23:05:19 -0700

    yc - WIP two of the four fields for UCSBOrganization form, plus story

---
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.js
@@ -0,0 +1,99 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+function UCSBOrganizationForm({initialContents, submitAction, buttonLabel = "Create"}) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialContents || {} });
+  // Stryker restore all
+
+  const navigate = useNavigate();
+
+  const testIdPrefix = "UCSBOrganizationForm";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      {initialContents && (
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="orgcode">OrgCode</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "-orgcode"}
+            id="orgcode"
+            type="text"
+            {...register("orgCode")}
+            value={initialContents.orgCode}
+            disabled
+          />
+        </Form.Group>
+      )}
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="orgtranslationshort">OrgTranslationShort</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-orgtranslationshort"}
+          id="orgtranslationshort"
+          type="text"
+          isInvalid={Boolean(errors.orgTranslationShort)}
+          {...register("orgTranslationShort", {
+            required: "OrgTranslationShort is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.orgTranslationShort?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="orgtranslation">OrgTranslation</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-orgtranslation"}
+          id="orgtranslation"
+          type="text"
+          isInvalid={Boolean(errors.orgTranslation)}
+          {...register("orgTranslation", {
+            required: "OrgTranslation is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.orgTranslation?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="inactive">Inactive</Form.Label>
+        <Form.Select
+            id="inactive"
+            data-testid={testIdPrefix + "-inactive"}
+            isInvalid={Boolean(errors.inactive)}
+            {...register("inactive", {
+                required: "Inactive is required.",
+            })}
+        >
+            <option value="">Select...</option>
+            <option value="true">True</option>
+            <option value="false">False</option>
+        </Form.Select>
+        <Form.Control.Feedback type="invalid">
+            {errors.inactive?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
+      <Button
+        variant="Secondary"
+        onClick={() => navigate(-1)}
+        data-testid={testIdPrefix + "-cancel"}
+      >
+        Cancel
+      </Button>
+    </Form>
+  );
+}
+
+export default UCSBOrganizationForm;
