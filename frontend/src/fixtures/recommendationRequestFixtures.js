a421fc47ca1fbf152145d6abd0618e8bfbce2940
jthampiratwong <jthampiratwong@ucsb.edu>
2025-05-07 16:23:58 -0700

    create fixtures for RecommendationRequest table

---
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -0,0 +1,43 @@
+const recommendationRequestFixtures = {
+    oneRecommendationRequest: {
+      "id": 4,
+      "requesterEmail": "perrytham@gmail.com",
+      "professorEmail": "pconrad@ucsb.edu",
+      "explanation": "hi1",
+      "dateRequested": "2025-05-07T00:00:00",
+      "dateNeeded": "2025-05-07T00:00:00",
+      "done": true
+    },
+    threeRecommendationRequests: [
+      {
+        "id": 4,
+        "requesterEmail": "perrytham@gmail.com",
+        "professorEmail": "pconrad@ucsb.edu",
+        "explanation": "hi1",
+        "dateRequested": "2025-05-07T00:00:00",
+        "dateNeeded": "2025-05-07T00:00:00",
+        "done": true
+      },
+      {
+        "id": 5,
+        "requesterEmail": "jirakit.tham@gmail.com",
+        "professorEmail": "pconrad@ucsb.edu",
+        "explanation": "hi2",
+        "dateRequested": "2025-05-07T00:00:00",
+        "dateNeeded": "2025-05-07T00:00:00",
+        "done": true
+      },
+      {
+        "id": 6,
+        "requesterEmail": "jthampiratwong@ucsb.edu",
+        "professorEmail": "pconrad@ucsb.edu",
+        "explanation": "hi3",
+        "dateRequested": "2025-05-07T00:00:00",
+        "dateNeeded": "2025-05-07T00:00:00",
+        "done": true
+      }
+    ],
+  };
+  
+  export { recommendationRequestFixtures };
+  
