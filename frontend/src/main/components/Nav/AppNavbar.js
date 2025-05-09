a32e629a582694b665a2485deeb0557d60e69a60
Lucas Havemose <lucashavemose@gmail.com>
2025-05-09 11:05:05 -0700

    Update AppNavbar.js
    forgot to include a </Nav.Link>

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -74,6 +74,7 @@ export default function AppNavbar({
                   </Nav.Link>
                   <Nav.Link as={Link} to="/ucsborganization">
                     UCSB Organizations
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/MenuItemReviews">
                     Menu Item Reviews
                   </Nav.Link>
